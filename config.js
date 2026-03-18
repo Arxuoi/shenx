c83cdf0e36e9d805e303248ed5ef3e61e4966f96
Arxuoi <108146566+Arxuoi@users.noreply.github.com>
2026-03-19 00:08:00 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    // API Naze
+    NAZE_API: {
+        baseUrl: 'https://api.naze.biz.id/ai/chat4',
+        apiKey: 'nz-55d9381817'  // API key Anda
+    },
+    
+    // Prefix untuk command bot
+    PREFIX: '.',
+    
+    // Owner bot (nomor Anda tanpa @c.us)
+    OWNER: '628892155121'
+};
